0fd9e13dae45f9be0e1bd3f177e46568dcff4202
abada <abedallaSK>
2023-08-25 23:27:58 +0300

    test

--- app/src/main/java/com/example/workv1/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/workv1/ui/home/HomeFragment.kt
@@ -1,19 +1,20 @@
 package com.example.workv1.ui.home
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.workv1.MainActivity
 import com.example.workv1.databinding.FragmentHomeBinding
-import com.example.workv1.health.HealthDataManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
+
 
 class HomeFragment : Fragment() {
 
@@ -24,7 +25,11 @@ private var _binding: FragmentHomeBinding? = null
 
   // This property is only valid between onCreateView and
   // onDestroyView.
+
+    var i = 0
   private val binding get() = _binding!!
+
+
 
   override fun onCreateView(
     inflater: LayoutInflater,
@@ -40,14 +45,36 @@ private var _binding: FragmentHomeBinding? = null
       val healthDataManager = (requireActivity() as MainActivity).healthDataManager
       val textView: TextView = binding.textHome
 
+     var progressBar = binding.progressBar
+      var  progressText = binding.progressText
+
+//      val handler = Handler()
+//      handler.postDelayed(object : Runnable {
+//          override fun run() {
+//              // set the limitations for the numeric
+//              // text under the progress bar
+//              if (i <= 100) {
+//                  progressText!!.text = "" + i
+//                  progressBar!!.progress = i
+//                  i++
+//                  handler.postDelayed(this, 200)
+//              } else {
+//                  handler.removeCallbacks(this)
+//              }
+//          }
+//      }, 200)
       mainScope.launch {
           healthDataManager.initializeHealthData()
             while (true) {
 
                 try {
-                   textView.setText( healthDataManager.readStepInputsForLastDay().sumBy { it.count.toInt() }.toString())
+                    var x=healthDataManager.readStepInputsForLastDay().sumBy { it.count.toInt() }
+                   textView.setText(x.toString())
+                    progressText!!.text = ((x*100/10000)).toString()+" _1"
+                    progressBar!!.progress=((x*100/10000))
                 }catch (e:Exception)
                 {
+
                     textView.setText(e.message)
                 }
 
